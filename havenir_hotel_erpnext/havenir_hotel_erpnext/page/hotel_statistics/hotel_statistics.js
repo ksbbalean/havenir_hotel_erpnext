20e0c41b775886966a405426901f90f1b82d9c61
mymi14s <mymi14s@hotmail.com>
2021-10-23 17:05:38 +0000

    Hotel Dashboard cont'd

--- havenir_hotel_erpnext/havenir_hotel_erpnext/page/hotel_statistics/hotel_statistics.js
+++ havenir_hotel_erpnext/havenir_hotel_erpnext/page/hotel_statistics/hotel_statistics.js
@@ -12,6 +12,7 @@ MyPage = Class.extend({
 			single_column: true
 		});
 		// make page
+		let $btn = this.page.set_primary_action('Refresh', () => this.get_data(), 'octicon octicon-plus')
 		this.make();
 	},
 
@@ -49,10 +50,14 @@ MyPage = Class.extend({
 	let content_ = `<h1>Dashboard</h1>`;
 
 		$(frappe.render_template(body, this)).appendTo(this.page.main);
+			// get data
+			this.get_data()
+	},
+	get_data: function(){
 		frappe.call({
-        method: "havenir_hotel_erpnext.havenir_hotel_erpnext.page.hotel_statistics.hotel_statistics.render", //dotted path to server method
-        callback: function(r) {
-            // code snippet
+				method: "havenir_hotel_erpnext.havenir_hotel_erpnext.page.hotel_statistics.hotel_statistics.render", //dotted path to server method
+				callback: function(r) {
+						// code snippet
 						document.querySelector('#_content').innerHTML = r.message.template
 						res = r.message;
 						google.charts.load('current', {'packages':['corechart']});
@@ -61,37 +66,34 @@ MyPage = Class.extend({
 						// Draw the chart and set the chart values
 						// start room occupancy
 						function roomOccupancy() {
-						  var data = google.visualization.arrayToDataTable([
-						  ['Room', 'Occupancy'],
-							['Reserved', res.reserved.reserved],
-						  ['Checked In', res.checked_in.checked_in],
-							['Available', res.available.available],
-							['Room Service', res.room_service.room_service]
+							var data = google.visualization.arrayToDataTable([
+							['Room', 'Occupancy'],
+							['Reserved', res.reserved],
+							['Checked In', res.checked_in],
+							['Available', res.available],
+							['Room Service', res.room_service]
 						]);
 
-// 						Available
-// Checked In
-// Reserved
-// Room Service
-						  // Optional; add a title and set the width and height of the chart
-						  var options = {'title':'Room Occupancy', 'width':280, 'height':100};
-						  // Display the chart inside the <div> element with id="piechart"
-						  var chart = new google.visualization.PieChart(document.getElementById('room_occupancy'));
-						  chart.draw(data, options);
+							// Optional; add a title and set the width and height of the chart
+							var options = {'title':'Room Occupancy', 'width':280, 'height':100};
+							// Display the chart inside the <div> element with id="piechart"
+							var chart = new google.visualization.PieChart(document.getElementById('room_occupancy'));
+							chart.draw(data, options);
 						}
 						// end room occupancy
 
 						// INITILIAZE TABLE
 							// update hous keeping table
-							console.log(res.house_keeping)
+							// console.log(res.house_keeping)
 							document.querySelector("#housekeeping-tbody").innerHTML = res.house_keeping;
-						    var table = $('#housekeeping-table').DataTable( {
-						        fixedHeader: true,
+								var table = $('#housekeeping-table').DataTable( {
+										fixedHeader: true,
 										"lengthMenu": [[5, 10, 20, -1], [5, 10, 20, "All"]]
-						    } );
+								} );
 
-        }
-    })
-		}
+				}
+		})
+	}
 	// end of class
+
 })
--- havenir_hotel_erpnext/havenir_hotel_erpnext/page/hotel_statistics/hotel_statistics.py
+++ havenir_hotel_erpnext/havenir_hotel_erpnext/page/hotel_statistics/hotel_statistics.py
@@ -6,22 +6,22 @@ def render():
 	checked_in = frappe.db.sql("""
 		SELECT COUNT(room_status) as checked_in
 		FROM `tabRooms` WHERE room_status='Checked In';
-	;""", as_dict=1)[0]
+	;""", as_dict=1)[0].checked_in
 
 	reserved = frappe.db.sql("""
 		SELECT COUNT(room_status) as reserved
 		FROM `tabRooms` WHERE room_status='Reserved';
-	;""", as_dict=1)[0]
+	;""", as_dict=1)[0].reserved
 
 	available = frappe.db.sql("""
 		SELECT COUNT(room_status) as available
 		FROM `tabRooms` WHERE room_status='Available';
-	;""", as_dict=1)[0]
+	;""", as_dict=1)[0].available
 
 	room_service = frappe.db.sql("""
 		SELECT COUNT(room_status) as room_service
 		FROM `tabRooms` WHERE room_status='Room Service';
-	;""", as_dict=1)[0]
+	;""", as_dict=1)[0].room_service
 
 
 
@@ -30,18 +30,19 @@ def render():
 	;""", as_dict=1)
 
 	context = {
-		'name': 'Ghorz'
+		'name': 'Ghorz',
+		'checked_in': checked_in,
+		'reserved': reserved,
+		'room_service': room_service,
+		'available': available,
 	}
 	template = frappe.render_template(
 		"havenir_hotel_erpnext/templates/includes/hotel_dashboard/index.html",
 		context=context)
 	return {
 		'template':template,
-		'checked_in': checked_in,
-		'reserved': reserved,
-		'room_service': room_service,
-		'available': available,
 		'house_keeping': frappe.render_template(
 			"havenir_hotel_erpnext/templates/includes/hotel_dashboard/housekepping_tbody.html",
-			context={'house_keeping':house_keeping})
+			context={'house_keeping':house_keeping}),
+		**context
 		}
