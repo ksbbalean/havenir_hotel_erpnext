1d4aaa2e227c321b79ca398b3d06c1c6eafaf939
mymi14s <mymi14s@hotmail.com>
2021-11-01 13:58:26 +0000

    Cont'd Restaurant Dashboard

--- havenir_hotel_erpnext/havenir_hotel_erpnext/page/restaurant_dashboard/restaurant_dashboard.py
+++ havenir_hotel_erpnext/havenir_hotel_erpnext/page/restaurant_dashboard/restaurant_dashboard.py
@@ -56,11 +56,12 @@ def render():
 		IFNULL(SUM(si.total_qty), 0) as qty,
 		IFNULL(SUM(si.is_takeout), 0) as is_takeout
 		FROM `tabSales Invoice` si
-		WHERE si.modified BETWEEN
-		"{str(datetime.today()).split(' ')[0]} 00:00:01"
-		AND "{str(datetime.today()).split(' ')[0]} 23:59:59"
-		AND si.docstatus=1;
+		WHERE si.docstatus=1;
 	;""", as_dict=1)
+
+# si.modified BETWEEN "{str(datetime.today()).split(' ')[0]} 00:00:01"
+# AND "{str(datetime.today()).split(' ')[0]} 23:59:59"
+
 
 	top_10_today = frappe.db.sql(f"""
 		SELECT i.item_code, sum(i.qty) as qty
@@ -71,6 +72,89 @@ def render():
 		GROUP BY i.item_code ORDER BY qty DESC LIMIT 10;
 	;""", as_dict=1)
 
+	# 	sales table
+	SALES_COUNT = []
+	SALES_COUNT_DICT = {}
+	SALES_VOLUME = []
+	SALES_VOLUME_DICT = {}
+	SALES_COUNT_TOTAL = {
+		'am8': 0, 'am10': 0, 'pm12': 0,
+		'pm2': 0, 'pm4': 0, 'pm6': 0,
+		'pm8': 0,
+	}
+	SALES_VOLUME_TOTAL = {
+		'am8': 0, 'am10': 0, 'pm12': 0,
+		'pm2': 0, 'pm4': 0, 'pm6': 0,
+		'pm8': 0,
+	}
+
+	SALESDATA = frappe.db.sql(f"""
+		SELECT si.item_code, si.qty, si.amount, s.modified
+		FROM `tabSales Invoice Item` si
+		JOIN `tabSales Invoice` s ON si.parent=s.name
+		WHERE s.docstatus=1 ORDER BY si.item_code ASC;
+	;""", as_dict=1)
+
+	for i in SALESDATA:
+		if(SALES_COUNT_DICT.get(i.item_code)):
+			pass
+		else:
+			SALES_COUNT_DICT[i.item_code] = {
+				'item_code':i.item_code,
+				'am8': 0, 'am10': 0, 'pm12': 0,
+				'pm2': 0, 'pm4': 0, 'pm6': 0,
+				'pm8': 0,
+			}
+			SALES_VOLUME_DICT[i.item_code] = {
+				'item_code':i.item_code,
+				'am8': 0, 'am10': 0, 'pm12': 0,
+				'pm2': 0, 'pm4': 0, 'pm6': 0,
+				'pm8': 0,
+			}
+
+		sales_time = i.modified.hour
+		if(sales_time<=9):
+			SALES_COUNT_DICT[i.item_code]['am8'] += i.qty
+			SALES_VOLUME_DICT[i.item_code]['am8'] += i.amount
+			SALES_COUNT_TOTAL['am8'] += i.qty
+			SALES_VOLUME_TOTAL['am8'] += i.amount
+		elif(sales_time<=11):
+			SALES_COUNT_DICT[i.item_code]['am10'] += i.qty
+			SALES_VOLUME_DICT[i.item_code]['am10'] += i.amount
+			SALES_COUNT_TOTAL['am10'] += i.qty
+			SALES_VOLUME_TOTAL['am10'] += i.amount
+		elif(sales_time<=13):
+			SALES_COUNT_DICT[i.item_code]['pm12'] += i.qty
+			SALES_VOLUME_DICT[i.item_code]['pm12'] += i.amount
+			SALES_COUNT_TOTAL['pm12'] += i.qty
+			SALES_VOLUME_TOTAL['pm12'] += i.amount
+		elif(sales_time<=15):
+			SALES_COUNT_DICT[i.item_code]['pm2'] += i.qty
+			SALES_VOLUME_DICT[i.item_code]['pm2'] += i.amount
+			SALES_COUNT_TOTAL['pm2'] += i.qty
+			SALES_VOLUME_TOTAL['pm2'] += i.amount
+		elif(sales_time<=17):
+			SALES_COUNT_TOTAL['pm4'] += i.qty
+			SALES_VOLUME_TOTAL['pm4'] += i.amount
+		elif(sales_time<=19):
+			SALES_COUNT_DICT[i.item_code]['pm6'] += i.qty
+			SALES_VOLUME_DICT[i.item_code]['pm6'] += i.amount
+			SALES_COUNT_TOTAL['pm6'] += i.qty
+			SALES_VOLUME_TOTAL['pm6'] += i.amount
+		else:
+			SALES_COUNT_DICT[i.item_code]['pm8'] += i.qty
+			SALES_VOLUME_DICT[i.item_code]['pm8'] += i.amount
+			SALES_COUNT_TOTAL['pm8'] += i.qty
+			SALES_VOLUME_TOTAL['pm8'] += i.amount
+
+
+	# move sales data to list
+	for i, j in SALES_COUNT_DICT.items():
+		SALES_COUNT.append(j)
+	for i, j in SALES_VOLUME_DICT.items():
+		SALES_VOLUME.append(j)
+
+
 	context = {
 		'table_occupancy_percent': round(
 			t_occupancy_percent, 2),
@@ -79,6 +163,10 @@ def render():
 		'total_sales_volume': frappe.format(total_sales[0].qty, 'Float'),
 		'is_takeout': frappe.format(total_sales[0].is_takeout, 'Float'),
 		'top_10_today': top_10_today,
+		'SALES_COUNT': SALES_COUNT,
+		'SALES_VOLUME': SALES_VOLUME,
+		'SALES_COUNT_TOTAL': [SALES_COUNT_TOTAL],
+		'SALES_VOLUME_TOTAL': [SALES_VOLUME_TOTAL],
 		# 'checked_in': checked_in,
 		# 'reserved': reserved,
 		# 'room_service': room_service,
